9d095a19a5a9f894bbc095f1e5daef93dfb1ad87
Sergo on HP <tulazis@yandex.ru>
2024-03-05 18:34:55 +0300

    Start application

--- index.php
+++ index.php
@@ -2,27 +2,24 @@
 
 require "settings.php";
 require "class/pdo.php";
-require "class/render.php";
+require "class/template.php";
 require "class/user.php";
 require "class/route.php";
 
 $route = new Route;
-$route->addItems(['theme', 'message', 'add']);
+$route->addItems(['themeslist', 'theme', 'message', 'add']);
 $connect = new Database;
 $user = new User($connect);
-$render = new Render($connect, $user);
-$path = htmlentities($_GET['mode']);
+
+$path = empty($_GET['mode'])? 'themeslist': trim(htmlentities($_GET['mode']));
 
 if ( !$route->check($path)) {
-    $render->showThemesList();
+    $path = 'tememslist';
 }
 else {
     if (!class_exists($path)){
         include "class/".$path.'.php';
     }
-    else {
-        $modeClass = new $path;
-    }
 
-    get_class($modeClass);
+    $modeClass = new $path($connect, $user);
 }
