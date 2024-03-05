85e09dd9584c81fc08d2ba0d8119c073ebc5e3f0
Sergo on HP <tulazis@yandex.ru>
2024-03-05 18:12:22 +0300

    Parent class for all renders

---
+++ class/template.php
@@ -0,0 +1,28 @@
+<?php
+
+class Template {
+
+    public
+        $db,
+        $user,
+        $tpl;
+
+    /**
+     * @param Database $connect
+     * @param User $user
+     */
+    public function __construct(Database $connect, User $user)
+    {
+        $this->db = $connect;
+        $this->user = $user;
+    }
+    /**
+     * @param $filename
+     * @return string
+     */
+    public function loadTpl($filename): string
+    {
+        return
+            file_get_contents('tpl/'.$filename);
+    }
+}
