f2dc78e3a0b622c0ffc4ff0026516293427a6a56
Sergo on HP <tulazis@yandex.ru>
2024-03-05 18:45:19 +0300

    comments

--- index.php
+++ index.php
@@ -7,10 +7,15 @@ require "class/user.php";
 require "class/route.php";
 
 $route = new Route;
+
+// перечисляем все допустимые роуты
 $route->addItems(['themeslist', 'theme', 'message', 'add']);
+
 $connect = new Database;
 $user = new User($connect);
 
+// целевой класс получаем в GET запросе через параметр mode
+// по умолчанию грузим список тематик
 $path = empty($_GET['mode'])? 'themeslist': trim(htmlentities($_GET['mode']));
 
 if ( !$route->check($path)) {
